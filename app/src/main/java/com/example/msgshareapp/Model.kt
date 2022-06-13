202d816af086d8c173493c2bd6f5fa5321e34dcc
Uthaya <uthaya24sss@gmail.com>
2022-06-13 23:44:00 +0530

    second commit

---
+++ app/src/main/java/com/example/msgshareapp/Model.kt
@@ -0,0 +1,37 @@
+package com.example.msgshareapp
+
+data class Hobby(var title: String)
+
+object Supplier{
+    val hobbies = listOf<Hobby>(
+        Hobby("Swimming"),
+        Hobby("Sleeping"),
+        Hobby("Reading"),
+        Hobby("Gaming"),
+        Hobby("Singing"),
+        Hobby("Dancing"),
+        Hobby("Running"),
+        Hobby("Swimming"),
+        Hobby("Sleeping"),
+        Hobby("Reading"),
+        Hobby("Gaming"),
+        Hobby("Singing"),
+        Hobby("Dancing"),
+        Hobby("Running"),
+        Hobby("Swimming"),
+        Hobby("Sleeping"),
+        Hobby("Reading"),
+        Hobby("Gaming"),
+        Hobby("Singing"),
+        Hobby("Dancing"),
+        Hobby("Running"),
+        Hobby("Swimming"),
+        Hobby("Sleeping"),
+        Hobby("Reading"),
+        Hobby("Gaming"),
+        Hobby("Singing"),
+        Hobby("Dancing"),
+        Hobby("Running")
+
+        )
+}
