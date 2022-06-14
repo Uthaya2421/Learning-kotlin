ca8a5505f2991eeccc37c39be6faecea7f9109c6
Uthaya <uthaya24sss@gmail.com>
2022-06-14 15:35:12 +0530

    initial commit

--- app/src/main/java/com/example/msgshareapp/HobbiesActivity.kt
+++ app/src/main/java/com/example/msgshareapp/HobbiesActivity.kt
@@ -2,6 +2,8 @@ package com.example.msgshareapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_hobbies.*
 
 class HobbiesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +11,11 @@ class HobbiesActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_hobbies)
 
+        val layoutManager = LinearLayoutManager(this)
+
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+        recyclerView.layoutManager = layoutManager
+        val adapter = HobbiesAdapter(this, Supplier.hobbies)
+        recyclerView.adapter = adapter
     }
 }
